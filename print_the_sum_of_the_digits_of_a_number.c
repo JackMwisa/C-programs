0dc40b5370939c740dddd92be3159691d7ee117a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-15 18:00:28 +0300

    sum digits of an integer

--- print_the_sum_of_the_digits_of_a_number.c
+++ print_the_sum_of_the_digits_of_a_number.c
@@ -2,11 +2,20 @@
 
 #include <stdio.h>
 
-int main(){
+int main() {
 
+    int num, sum = 0, remainder;
 
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
+    while (num != 0) {
+        remainder = num % 10;
+        sum += remainder;
+        num /= 10;
+    }
+    printf("Sum of digits: %d\n", sum);
 
-
+    return 0;
 
 }
